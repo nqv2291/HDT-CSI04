5942b9db0fa09644507637ab0d6c317ee5029b1f
Dương <minhduongqo@gmail.com>
2022-07-08 22:27:19 +0700

    a

--- DuongsSecretTestingPlace/iterateSearch.py
+++ DuongsSecretTestingPlace/iterateSearch.py
@@ -1,4 +1,8 @@
-def fac(n):
-    return n*fac(n-1) if n > 1 else 1
-
-print(fac(1))
+words = [
+    'abattoir', 
+    'ablution', 
+    'acerbic', 
+    'acquiescence', 
+    'acrostic', 
+    'acrosticice'
+]
